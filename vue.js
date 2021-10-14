c9187c164a26455d563461bbeedd49de0c79c027
kabbya5 <57331857+kabbya5@users.noreply.github.com>
2021-10-14 23:56:08 +0600

    Add files via upload

--- vue.js
+++ vue.js
@@ -1,32 +1,11 @@
 new Vue({
   el: ".element",
   data:{
-    title: "I live in bangladesh",
-    isIttrue: true,
-    car:[
-      'ford',
-      'toyota',
-      'lamborgini',
-    ],
-    object:{
-      name: 'man',
-      age: 32,
-      city: 'bangladesh',
-    },
-    gretting: ()=> {
-       return "hello world";
-    },
-    hello(){
-      return this.title;
-    },
-    add(){
-      return this.title + " " + this.car[2];
-    }
-  },
-  methods:{
-    green: function(){
-      return " hello vue"
+    title: " Today We will learn directive",
+    src: "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature.jpg?sfvrsn=64942d53_4",
+    alt: "some frout Images",
+    link:"http://www.facebook.com",
 
-    }
   }
+
 });
